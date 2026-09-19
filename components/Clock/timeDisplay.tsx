279d6a238c0ed7c8bfa3d84e4d163f1f4aa4bfcc
Pratik <ipratiklimbekar@gmail.com>
2026-09-19 12:12:20 +0530

    added pwa functionality

--- components/Clock/timeDisplay.tsx
+++ components/Clock/timeDisplay.tsx
@@ -3,7 +3,7 @@ import '@/components/Clock/clock.css';
 export default function TimeDisplay({ time } : {time : Date | null }) {
     const currentTime = time?.toLocaleTimeString('en-US', {
         hour: '2-digit',
-        minute: '2-digit', second: '2-digit', hour12: false
+        minute: '2-digit', second: '2-digit', hour12: false //change this to true to implement 12 hr format
     });
     if (currentTime) {
     return (<div className='timeClock'> {currentTime} </div>)
