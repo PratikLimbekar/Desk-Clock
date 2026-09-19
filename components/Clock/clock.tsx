3a597f2865184480e383a506a1c6e8e63cc7a348
Pratik <ipratiklimbekar@gmail.com>
2026-09-19 13:09:52 +0530

    fixed manifest.json, added icons acc to chrome installability criteria and removed install button

--- components/Clock/clock.tsx
+++ components/Clock/clock.tsx
@@ -2,13 +2,11 @@
 import TimeDisplay from "./timeDisplay";
 import DateDisplay from "./dateDisplay";
 import { useClock } from "@/hooks/useClock";
-import InstallButton from "../installButton";
 
 export default function Clock() {
     const date = useClock();
     return (
         <div className="mainClock">
-            <InstallButton></InstallButton>
             <TimeDisplay time={date}></TimeDisplay>
             <DateDisplay date={date}></DateDisplay>
         </div>
